d31029e79468c06ce1fc7ba4da17623a2665f635
Nicholai Hansen <nicholaihansen1993@gmail.com>
2020-01-15 16:35:13 -0800

    bootstrap modal update and image bug fix

--- tvmaze.js
+++ tvmaze.js
@@ -21,20 +21,32 @@ async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  
 
   // Search API with Axios with our query
-  let searchObjects = (await axios.get('http://api.tvmaze.com/search/shows', { params: { q: query } })).data
+  let searchObjects = (await axios.get('http://api.tvmaze.com/search/shows', {
+    params: {
+      q: query
+    }
+  })).data
 
   let showList = [];
 
   // ASK WHY for (let of searchObjects) yields type error for accessing .show
   for (let searchObject of searchObjects) {
-    let { id, name, summary, image } = searchObject.show;
+    let image = searchObject.show.image ? searchObject.show.image.medium : 
+    "https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1" +
+    "ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300";
+    
+    let {
+      id,
+      name,
+      summary,
+    } = searchObject.show;
     // Initialize show object
 
     let showObj = {
       id,
       name,
       summary,
-      image: image.medium || "https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300"
+      image 
     }
 
     // Insert show object into array of shows that match query.
@@ -59,13 +71,15 @@ function populateShows(shows) {
   for (let show of shows) {
     let $item = $(
       `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
+         <div class="card" data-show-id="${show.id}" data-show-name="${show.name}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <img class="card-img-top" src=${show.image}>
              <p class="card-text">${show.summary}</p>
            </div>
-           <button class="show-episodes">Show Episodes</button>
+           <button type="button" class="show-episodes btn btn-primary" 
+           data-toggle="modal" data-target="#exampleModalLong">
+           Show Episodes</button>
          </div>
        </div>
       `);
@@ -74,6 +88,12 @@ function populateShows(shows) {
   }
 
   $("#shows-list").on("click", ".show-episodes", async function (e) {
+
+    // Display title of episode
+    let title = $(e.target).parent().data().showName;
+    $('#exampleModalLongTitle').text(`Episodes of ${title}`);
+
+    // Display episodes of targeted show
     let id = $(e.target).parent().data().showId;
     let episodeList = await getEpisodes(id);
     populateEpisodesList(episodeList);
@@ -115,7 +135,12 @@ async function getEpisodes(id) {
   let episodeList = [];
 
   for (let episode of episodes) {
-    let { id, name, season, number } = episode;
+    let {
+      id,
+      name,
+      season,
+      number
+    } = episode;
 
     let episodeObj = {
       id,
@@ -131,6 +156,7 @@ async function getEpisodes(id) {
   // TODO: return array-of-episode-info, as described in docstring above
 }
 
+// Function that takes in list of episodes and displays them to user
 function populateEpisodesList(episodeList) {
 
   let $episodeContainer = $("#episodes-list");
